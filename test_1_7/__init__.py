23a235af2c6d912499f51d9bab721da618cb649f
zanweicr <79506551+zanweicr@users.noreply.github.com>
2022-01-09 22:30:55 +0800

    Add files via upload

---
+++ test_1_7/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# @Time    : 2022/01/07 23:00
+# @Author  : makabaka
+# @Version : python3.9
+# @File    : __init__.py 
+
---
+++ test_1_7/while.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# @Time    : 2022/01/07 23:00
+# @Author  : makabaka
+# @Version : python3.9
+# @File    : while 
+
+
+i = 0
+
+while i < 10:
+    print("你好")
+    i += 1
+
