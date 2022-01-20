dae1a84d460e5c6b661aca4012682660627cb171
zanweicr <79506551+zanweicr@users.noreply.github.com>
2022-01-20 16:22:00 +0800

    Add files via upload

---
+++ test_1_14/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# @Time    : 2022/01/14 22:09
+# @Author  : makabaka
+# @Version : python3.9
+# @File    : __init__.py 
+
---
+++ test_1_14/fun2.py
@@ -0,0 +1,47 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+# @Time    : 2022/01/15 9:28
+# @Author  : makabaka
+# @Version : python3.9
+# @File    : fun2 
+
+# def add_num1(a, b):
+#     result = a+b
+#     print(result)
+#
+#
+# add_num1(10, 20)
+
+# def buy(cigarette_price):
+#     """
+#     香烟购买函数
+#     :param cigarette_price: 香烟价格
+#     :return: 返回具体名字
+#     """
+#     if cigarette_price == 12:
+#         return '磨砂'
+#     if cigarette_price == 8:
+#         return '软延安'
+#     if cigarette_price == 19:
+#         return '煊赫门'
+#     if cigarette_price == 13:
+#         return '云烟'
+#     if cigarette_price == 18:
+#         return '黑兰州'
+#
+#
+# goods = buy(18)
+# print(goods)
+# #
+# # help(len)
+# help(buy)
+
+# def average_num(a, b, c):
+#     return (a + b + c) / 3
+#
+#
+# num = average_num(20, 30, 40)
+# print(num)
+a = 1
+b = 1
+print(id(a))
